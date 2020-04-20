a348f60dd6d4cb70e9cd32c71aa6875ce98f3419
mhaig1 <62253091+mhaig1@users.noreply.github.com>
2020-04-20 15:17:29 -0400

    Add files via upload

--- json_exercise.py
+++ json_exercise.py
@@ -2,15 +2,12 @@ import urllib.request
 import json
 import pprint
 
-APIKEY = 'YOUR_OWN_APIKEY'
+APIKEY = '435540007afd43d6062e3cf0f7e95c64'
 city = 'Wellesley'
 country_code = 'us'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country_code}&APPID={APIKEY}'
 
-# print(url)
 f = urllib.request.urlopen(url)
 response_text = f.read().decode('utf-8')
 response_data = json.loads(response_text)
-pprint.pprint(response_data)
-
-# Can you get the current temperature in Wellesley?
+pprint.pprint(response_data['main']['temp'])
